a51166800a4fe6ac331170d1aeb0cea273db34f8
Rui Favinha <ruifavinha2002@gmail.com>
2018-06-30 23:50:09 +0100

    Updated

--- Arithmatic Quiz 1.py
+++ Arithmatic Quiz 1.py
@@ -6,17 +6,19 @@ choice_1 = 0
 guess = 0
 
 
-def create_q( operation, choice, choice_1, guess, question ):
-        question +=1
+def create_q( operation, choice, choice_1, guess):
         choice = random.randint(0,10)
         choice_1 = random.randint(0,10)
         operation = random.randint(0,2)
+
         if operation == 0:
                 operation = "plus"
                 answer( operation, choice, choice_1, guess, score )
+
         elif operation == 1:
                 operation = "minus"
                 answer( operation, choice, choice_1, guess, score )
+
         elif operation == 2:
                 operation = "times"
                 answer( operation, choice, choice_1, guess, score )
@@ -25,50 +27,49 @@ def create_q( operation, choice, choice_1, guess, question ):
 
 
 def answer( operation, choice, choice_1, guess, score ):
+
         if operation == "plus":
                 print ("What is,",choice,operation,choice_1,"?")
                 guess = int(input())
                 answer = choice + choice_1
+
                 if guess == answer:
                         print("Congrats! You got it right!")
                         score += 1
                         print("The score now is:",score)
-                        create_q( operation, choice, choice_1, guess, question )
 
                 else:
                         print("Wrong!")
                         print("The answer was:",answer)
-                        create_q( operation, choice, choice_1, guess, question )
 
         elif operation == "minus":
                 print ("What is,",choice,operation,choice_1,"?")
                 guess = int(input())
                 answer = choice - choice_1
+
                 if guess == answer:
                         print("Congrats! You got it right!")
                         score += 1
                         print("The score now is:",score)
-                        create_q( operation, choice, choice_1, guess, question )
 
                 else:
                         print("Wrong!")
                         print("The answer was:",answer)
-                        create_q( operation, choice, choice_1, guess, question )
 
         elif operation == "times":
                 print ("What is,",choice,operation,choice_1,"?")
                 guess = int(input())
                 answer = choice * choice_1
+
                 if guess == answer:
                         print("Congrats! You got it right!")
                         score += 1
                         print("The score now is:",score)
-                        create_q( operation, choice, choice_1, guess, question )
 
                 else:
                         print("Wrong!")
                         print("The answer was:",answer)
-                        create_q( operation, choice, choice_1, guess, question )
+
 
 
 
@@ -80,7 +81,7 @@ def answer( operation, choice, choice_1, guess, score ):
 print("Welcome to the quiz!")
 name = input("Enter Your Name: ")
 print("Hello,",name,", let's start the quiz!")
-question = 0
-while question < 9:
-    create_q( operation, choice, choice_1, guess, question )
+for x in range(9):
+    create_q( operation, choice, choice_1, guess)
+
 print("Your total for the test was:",score,"/10.")
