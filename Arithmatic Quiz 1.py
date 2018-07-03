82ed8ea58a1e044407270187dea5985f853bd8ca
Rui Favinha <ruifavinha2002@gmail.com>
2018-07-03 19:22:09 +0100

    Added comments

--- Arithmatic Quiz 1.py
+++ Arithmatic Quiz 1.py
@@ -1,4 +1,6 @@
 import random
+
+#Set all the variables
 score, operation, choice, choice_1, guess = 0, 0, 0, 0, 0
 
 print("Welcome to the quiz!")
@@ -6,14 +8,18 @@ name = input("Enter Your Name: ")
 print("Hello,",name,"!")
 print ("Now, what class are you in?")
 classs = input()
+#Name and class is to be used later
 print("Just to make sure, you are:",name,"and your class is:",classs)
 print("Let's start the quiz!")
-
+#Start a for loop to create the questions
 for x in range(9):
+    #Select a random number from one to ten
      choice = random.randint(0,10)
+    #Select a random number from one to ten
      choice_1 = random.randint(0,10)
      operation = random.randint(0,2)
 
+#Select an operation at random
      if operation == 0:
              operation = "plus"
              print ("What is,",choice,operation,choice_1,"?")
@@ -22,11 +28,12 @@ for x in range(9):
 
              if guess == answer:
                      print("Congrats! You got it right!")
-                     #global score
+                     #If the answer was right add one to score
                      score = score + 1
                      print("The score now is:",score)
 
              else:
+                    #Else print wrong
                      print("Wrong!")
                      print("The answer was:",answer)
 
@@ -53,7 +60,6 @@ for x in range(9):
 
              if guess == answer:
                      print("Congrats! You got it right!")
-                     #global score
                      score = score + 1
                      print("The score now is:",score)
              else:
@@ -61,7 +67,9 @@ for x in range(9):
                      print("The answer was:",answer)
 
 
+#Print the score the user got
 print("Your total for the test was:",score,"/10.")
+#Write score to a text file
 out_file = open(classs + name +".txt", "wt")
 out_file.write("Score:",str(score))
 out_file.close()
